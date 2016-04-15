4c4be95de863243ec7ed5af22e737f4ac2ef0fa0
wanglei25 <624432528@qq.com>
2016-04-15 11:19:58 +0800

    https client test

--- src/test/java/com/https/busi/utils/httpclient/HttpClientTest.java
+++ src/test/java/com/https/busi/utils/httpclient/HttpClientTest.java
@@ -3,19 +3,45 @@ package com.https.busi.utils.httpclient;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.Asserts;
 import org.apache.http.util.EntityUtils;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by wanglei25 on 2016/4/14.
  */
 public class HttpClientTest {
 
+
     @Test
-    public void testHttpClient(){
-        HttpClient httpClient = new HttpsClientUtil().createSSLClient();
+    public void testMain(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a number(0: Get Https 1:Get Http):");
+        int s = scanner.nextInt();
+        HttpClient httpClient = null;
+        switch (s){
+            case 0:
+                httpClient = new HttpsClientUtil().createSSLClient();
+                break;
+            case 1:
+                httpClient = HttpClients.createDefault();
+                break;
+            default:
+                System.out.println("Wrong Number !!");
+        }
+        if (httpClient != null){
+            testHttpsClient(httpClient);
+        }
+    }
+
+    public void testHttpsClient(HttpClient httpClient){
         HttpGet get = new HttpGet("https://localhost:8447/doLogic/todo");
         try {
             HttpResponse response = httpClient.execute(get);
@@ -28,4 +54,5 @@ public class HttpClientTest {
             get.releaseConnection();
         }
     }
+
 }
